ec18c2b308dcb054205d7364a3a63bd811664661
Masterkedi <yunusefeersoz@hotmail.com>
2023-02-03 16:28:07 +0300

    A

--- GGJ/Assets/31.cs
+++ GGJ/Assets/31.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 31 : MonoBehaviour
+public class Yunus : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
