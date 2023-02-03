1cdbe7d3faa7e298c9bc7b6dcf7955af9b2a6496
Masterkedi <yunusefeersoz@hotmail.com>
2023-02-03 23:01:54 +0300

    A

--- GGJ/Assets/Chase.cs
+++ GGJ/Assets/Chase.cs
@@ -7,9 +7,16 @@ public class Chase : MonoBehaviour
     public GameObject OrtaTile;
     public float speed;
     private float distance;
+    
+
+    
     void Start()
     {
         
+        Vector2 Rot = new Vector2(OrtaTile.transform.position.x, OrtaTile.transform.position.y);
+        Vector2 lookDir = Rot - GetComponent<Rigidbody2D>().position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.Euler(0,0,angle);
     }
 
     // Update is called once per frame
