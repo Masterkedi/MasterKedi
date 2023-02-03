d315615edbfe3f756e0e339f9b8c8d5dd96b39b8
Masterkedi <yunusefeersoz@hotmail.com>
2023-02-03 20:11:34 +0300

    y

---
+++ GGJ/Assets/Tiledöndürme.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiledöndürme : MonoBehaviour
+{
+    public void TileRotate() 
+    {
+        transform.Rotate(0, 0, 90);
+        Debug.Log("HAHAHA");
+    }
+}
